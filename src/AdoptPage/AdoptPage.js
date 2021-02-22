7c214fc456a5b3c362d84a7552a0f927ca644e8a
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-21 19:39:25 -0800

    added form on change and dequeue functions

--- src/AdoptPage/AdoptPage.js
+++ src/AdoptPage/AdoptPage.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import AnimalCard from '../AnimalCard/AnimalCard';
 import AdoptionQueue from '../AdoptionQueue/AdoptionQueue';
 import ApiService from '../Services/ApiService';
-import logo from './petful-icon.png'
+import logo from './petful-icon.png';
 import './AdoptPage.css';
 
 const queueNames = [
   'Greg McCoy',
   'Lucy Boneparte',
   'Jason Myerzonne',
-  'Edith Rose'
-]
+  'Edith Rose',
+];
 
 class AdoptPage extends Component {
   state = {
@@ -29,38 +29,64 @@ class AdoptPage extends Component {
         this.setState({ cat });
       })
       .catch((error) => this.setState({ error }));
-
     ApiService.getDog()
       .then((dog) => {
         this.setState({ dog });
       })
       .catch((error) => this.setState({ error }));
-
     ApiService.getPeople()
       .then((people) => {
         this.setState({ people });
       })
       .catch((error) => this.setState({ error }));
-
   }
+
+  dequeuePet = () => {
+    let randoBool = Math.random() < 0.5;
+    if (randoBool === true) {
+      ApiService.dequeueCat().then(() => {
+        ApiService.getCat()
+          .then((cat) => {
+            this.setState({ cat });
+          })
+          .catch((error) => this.setState({ error }));
+      });
+    } else {
+      ApiService.dequeueDog().then(() => {
+        ApiService.getDog()
+          .then((dog) => {
+            this.setState({ dog });
+          })
+          .catch((error) => this.setState({ error }));
+      });
+    }
+  };
+
+  handleFormChange = (e) => {
+    e.preventDefault();
+    this.setState({ person: e.target.value });
+  };
+
   render() {
     return (
       <section className="adoptWindow">
-        <div className='headerBox'>
-          <img className='pLogo' src={logo} alt='Petful Icon'></img>
-        <h1 className="adoptTitle">Petful</h1>
+        <div className="headerBox">
+          <img className="pLogo" src={logo} alt="Petful Icon"></img>
+          <h1 className="adoptTitle">Petful</h1>
         </div>
         <div className="animalCardBox">
           <AnimalCard pet={this.state.cat} />
           <AnimalCard pet={this.state.dog} />
         </div>
-        <form>
-          <label htmlFor="nameInput">
-            Wanna join the queue? Add your name below:
-          </label>
-          <input id="nameInput" type="text"></input>
-          <button type="submit">Submit</button>
-        </form>
+        <div className="queueFormBox">
+          <form className="queueForm">
+            <label htmlFor="nameInput">
+              Wanna join the queue? Add your name below:
+            </label>
+            <input id="nameInput" type="text" onChange={(e) => this.handleFormChange(e)}></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
         <AdoptionQueue />
       </section>
     );
--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -12,6 +12,7 @@ const ApiService = {
       })
       .catch((error) => console.error(error));
   },
+
   getDog() {
     return fetch(`${REACT_APP_API_BASE}/dogs`)
       .then((res) => {
@@ -23,6 +24,7 @@ const ApiService = {
       })
       .catch((error) => console.error(error));
   },
+
   getPeople() {
     return fetch(`${REACT_APP_API_BASE}/people`)
       .then((res) => {
@@ -33,6 +35,67 @@ const ApiService = {
         }
       })
       .catch((error) => console.error(error));
+  },
+
+  queueUser(name) {
+    return fetch(`${REACT_APP_API_BASE}/people`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ person: name })
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return res.json().then((e) => Promise.reject(e));
+      } else {
+        return res.json();
+      }
+    })
+    .catch((error) => console.error(error));
+  },
+
+  dequeuePerson() {
+    return fetch(`${REACT_APP_API_BASE}/people`, {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' }
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return res.json().then((e) => Promise.reject(e));
+      } else {
+        return res.json();
+      }
+    })
+    .catch((error) => console.error(error));
+  },
+
+  dequeueCat() {
+    return fetch(`${REACT_APP_API_BASE}/cats`, {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' }
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return res.json().then((e) => Promise.reject(e));
+      } else {
+        return res.json();
+      }
+    })
+    .catch((error) => console.error(error));
+  },
+  
+  dequeueDog() {
+    return fetch(`${REACT_APP_API_BASE}/dogs`, {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' }
+    })
+    .then((res) => {
+      if (!res.ok) {
+        return res.json().then((e) => Promise.reject(e));
+      } else {
+        return res.json();
+      }
+    })
+    .catch((error) => console.error(error));
   },
 };
 
